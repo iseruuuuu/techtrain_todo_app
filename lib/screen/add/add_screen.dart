2fc5cb6837c87ce96e4b91299b14119719fbb954
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-12-24 13:41:09 +0900

    add controllerを繋げた

---
+++ lib/screen/add/add_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:techtrain_todo_app/component/add_button.dart';
+import 'package:techtrain_todo_app/component/add_item.dart';
+import 'package:techtrain_todo_app/component/add_textfield.dart';
+import 'package:techtrain_todo_app/component/add_title_item.dart';
+import 'package:techtrain_todo_app/screen/add/add_screen_controller.dart';
+
+class AddScreen extends StatelessWidget {
+  const AddScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(AddScreenController(), tag: '');
+    final deviceSize = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: const Text(
+          '新規作成',
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 17,
+            fontStyle: FontStyle.normal,
+            color: Colors.black,
+          ),
+        ),
+        leading: IconButton(
+          onPressed: controller.onTap,
+          icon: const Icon(
+            Icons.close,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: const [
+          AddItem(
+            title: 'タスク名',
+            hintText: '20文字いないで入力してください',
+          ),
+          AddItem(
+            title: '期日',
+            hintText: '年/月/日',
+          ),
+          AddItem(
+            title: 'カテゴリー',
+            hintText: '年/月/日',
+          ),
+          AddItem(
+            title: '詳細',
+            hintText: '入力してください',
+          ),
+          Center(child: AddButton()),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screen/add/add_screen_controller.dart
@@ -0,0 +1,19 @@
+import 'package:get/get.dart';
+
+class AddScreenController extends GetxController {
+
+
+  @override
+  void onInit() {
+    super.onInit();
+
+  }
+
+  void _sharedPreference() async {
+  }
+
+
+  void onTap() {
+    Get.back();
+  }
+}
